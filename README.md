1066d0d9af000c4f3fb9740d21e5243504633a96
philoniare <philoniare@gmail.com>
2016-01-31 21:38:58 -0500

    hello world in perl

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # Language adventures
 My personal notes on learning programming languages
 
-# Assumptions
+# Notes on notes
 I know a few languages already and that would probably color my comments
-and thoughts as I explore the languages. Therefore, I might skip a few thought
-nodes as I add new nodes to my tree of knowledge.
+and thoughts as I explore the languages. Therefore, if you are relatively new,
+you might find it a bit hard to keep up. I think each learning journey is
+unique because we have different starting points. Feel free to digress and
+look up information or speed whenever appropriate.
 
 # Approach
 It is usually better to have some sort of guide at the beginning so that the
@@ -14,5 +16,6 @@ if there is one available for that language.
 
 
 # Programming languages covered:
-- perl
--- http://qntm.org/files/perl/perl.html
+- perl. Guide: http://qntm.org/files/perl/perl.html
+Files:
+  intro.pl
--- intro.pl
+++ intro.pl
@@ -2,5 +2,44 @@
 # Author: philoniare
 # Last Updated: 1/31/2016
 
+## I needed to learn some perl to make an IA for DuckDuckGo
 
-# Install perl at: https://www.perl.org/get.html
+## Some background on perl:
+# Perl is the father of scripting languages, first designed by Larry Wall
+# in 1987. A lot of modern scripting langugas have its roots in perl and
+# in some cases the syntax is quite identical. The camel logo made by O'Reilly
+# has become the unofficial logo of the language. By the way, here's the
+# reason why O'Reilly has animals all over their book covers:
+# http://archive.oreilly.com/pub/a/oreilly//news/ediemals_0400.html
+
+# At the time of this writing, perl is steadily on decline with criticisms
+# such as 'write-only language'. I am going in pretty optimistic about it.
+# As long as the code is written well and with good intent, the general
+# syntax wouldn't bother me too much. It's not like developers are
+# intentionally obfuscating code like in International Obfuscated C
+# Code Contest. Here's an example: http://www.ioccc.org/2014/birken/prog.c
+
+## Install perl at: https://www.perl.org/get.html
+#! Comments are simular to py
+
+use strict;             # looks like js got the statement from here...
+use warnings;
+# similar to js, perl is a language that is easy to shoot yourself
+# in the foot and strict warnings are also helpful for big projects
+
+print "hello world";    # the CS tradition
+
+## Data Types
+#! Scalars -> undef, number, string, reference
+# are we also going to have vector types?
+# I do see quite a bit of resemblance with js (undef-undefined)
+# but references, we might be here for some pointer roller coaster ride
+
+my $def = undef;  # variables are declared with my, like js var
+print $def;       # raises a warning only if use warnings is on
+my $num = 40.5;
+print $num;
+my $num1 = $num;  # probably a copy by value for atomic data types
+print $num1;
+
+print "Hello " . $num1;   # . string concat is quite nostalgic from php
